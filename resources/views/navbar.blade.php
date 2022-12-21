7c8c8ebcf539a2c1e8416be8ab1e642a61d44b14
Ashish kumar <ashishkumar739293@gmail.com>
2022-12-21 16:02:50 +0530

    first commit

---
+++ resources/views/navbar.blade.php
@@ -0,0 +1,13 @@
+<link href="../css/style.css" rel="stylesheet" type="css/text" />
+<div class="nav">
+    <ul>
+        <a href="#"><li>Home</li></a>
+        <a href="#"><li>AboutUs</li></a>
+        <a href="#"><li>Register</li></a>
+        <a href="#"><li>Login</li></a>
+        <a href="#"><li>Course</li></a>
+        <a href="#"><li>Gallery</li></a>
+        <a href="#"><li>Staff</li></a>
+        <a href="#"><li>Contact</li></a>
+    </ul>
+</div>
